0dad760e9bda5152a3c3b73e9784f3d1fa387f97
Van Liem <vanliem2204@gmail.com>
2016-06-30 11:27:33 +0700

    Code migration

---
+++ database/migrations/2016_06_30_041445_create_relationships_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRelationshipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('relationships', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('following_id')->unsigned();
+            $table->foreign('following_id')->references('id')->on('users');
+            $table->integer('follower_id')->unsigned();
+            $table->foreign('follower_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('relationships');
+    }
+}
